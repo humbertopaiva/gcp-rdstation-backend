82336e213f76f88f416b6279512f7100cb221bd1
Humberto Paiva <humbertomoreira93@gmail.com>
2023-04-22 16:45:48 -0300

    teste

--- sh/sync_db.sh
+++ sh/sync_db.sh
@@ -13,7 +13,7 @@ LOCAL_DATABASE="wordpress"
 
 # Criação do arquivo de dump do banco de dados remoto
 echo "Criando o dump do banco de dados remoto..."
-mysqldump -u $REMOTE_USER -p$REMOTE_PASSWORD --host $REMOTE_HOST -P $REMOTE_PORT --single-transaction --column-statistics=0 --set-gtid-purged=OFF $REMOTE_DATABASE > backup.sql
+mysqldump -u $REMOTE_USER -p$REMOTE_PASSWORD --host $REMOTE_HOST -P $REMOTE_PORT --single-transaction --column-statistics=0 --set-gtid-purged=OFF $REMOTE_DATABASE > db_backup/backup.sql
 
 # Verifica se o arquivo de dump foi criado com sucesso
 if [ $? -ne 0 ]; then
@@ -23,7 +23,7 @@ fi
 
 # Restaura o dump no banco de dados local
 echo "Restaurando o dump no banco de dados local..."
-docker-compose exec -T db mysql -u $LOCAL_USER -p$LOCAL_PASSWORD $LOCAL_DATABASE < backup.sql
+docker-compose exec -T db mysql -u $LOCAL_USER -p$LOCAL_PASSWORD $LOCAL_DATABASE < db_backup/backup.sql
 
 # Verifica se o processo de restore foi bem-sucedido
 if [ $? -eq 0 ]; then
@@ -31,3 +31,7 @@ if [ $? -eq 0 ]; then
 else
   echo "Erro ao restaurar o dump no banco de dados local."
 fi
+
+# Remove o arquivo de backup
+rm db_backup/backup.sql
+
