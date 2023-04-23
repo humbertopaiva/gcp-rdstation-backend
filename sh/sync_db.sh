20e5a1ed1f4e9d96740bb14de54183f412c432bb
Humberto Paiva <humbertomoreira93@gmail.com>
2023-04-22 21:02:36 -0300

    push

--- sh/sync_db.sh
+++ sh/sync_db.sh
@@ -3,17 +3,24 @@
 # Configuração das variáveis de conexão
 REMOTE_USER=root
 REMOTE_PASSWORD=sqlpassword
-REMOTE_HOST=127.0.0.1
-REMOTE_PORT=3306
+REMOTE_INSTANCE_CONNECTION_NAME="headless-site:us-central1:sql-instance"
 REMOTE_DATABASE=wordpress
 
 LOCAL_USER="root"
 LOCAL_PASSWORD="sqlpassword"
 LOCAL_DATABASE="wordpress"
 
+CREDENTIALS_FILE="./secrets/cloudsql-proxy/credentials.json"
+
+# Inicializa o cloudsql-proxy em background
+./cloud-sql-proxy --address 0.0.0.0 --port 1234 headless-site:us-central1:sql-instance -credential_file=./secrets/cloudsql-proxy/credentials.json
+
+# Aguarda o cloudsql-proxy iniciar
+sleep 5
+
 # Criação do arquivo de dump do banco de dados remoto
 echo "Criando o dump do banco de dados remoto..."
-mysqldump -u $REMOTE_USER -p$REMOTE_PASSWORD --host $REMOTE_HOST -P $REMOTE_PORT --single-transaction --column-statistics=0 --set-gtid-purged=OFF $REMOTE_DATABASE > db_backup/backup.sql
+mysqldump -u $REMOTE_USER -p$REMOTE_PASSWORD --host 127.0.0.1 -P 3306 --single-transaction --column-statistics=0 --set-gtid-purged=OFF $REMOTE_DATABASE > db_backup/backup.sql
 
 # Verifica se o arquivo de dump foi criado com sucesso
 if [ $? -ne 0 ]; then
@@ -35,3 +42,5 @@ fi
 # Remove o arquivo de backup
 rm db_backup/backup.sql
 
+# Finaliza o cloudsql-proxy
+kill $CLOUDSQL_PROXY_PID
